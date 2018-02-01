169a880fb6952c6ef6516b11f39af1cc54eaaa2d
Sharmarke Duale <sdua1301@gmail.com>
2018-01-31 19:19:52 -0600

    lesson-01 goal tracking app

---
+++ goalpost-app/Controller/GoalsVC.swift
@@ -0,0 +1,27 @@
+//
+//  GoalsVC.swift
+//  goalpost-app
+//
+//  Created by nopcallowed on 1/31/18.
+//  Copyright © 2018 GASHAN INC. All rights reserved.
+//
+
+import UIKit
+
+class GoalsVC: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    @IBAction func addGoalBtnWasPressd(_ sender: Any) {
+        print("button was pressed")
+    }
+    
+
+}
+
